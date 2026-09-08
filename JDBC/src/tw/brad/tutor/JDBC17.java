b26d16da2a79d4963d3f93c338e3dfc8d17497ce
qinyan <qinyan60229@gmail.com>
2026-09-09 02:09:52 +0800

    09090202

--- JDBC/src/tw/brad/tutor/JDBC17.java
+++ JDBC/src/tw/brad/tutor/JDBC17.java
@@ -18,7 +18,7 @@ public class JDBC17 {
 	// Application => 共用 Connection Pool
 	private static HikariDataSource dataSource;
 	
-	static {
+	static {      //static initialization block 靜態初始化區塊   class 第一次被載入時，只會執行一次
 		HikariConfig config = new HikariConfig();
 		config.setJdbcUrl("jdbc:mysql://localhost:3306/brad");
 		config.setUsername("root");
@@ -44,8 +44,8 @@ public class JDBC17 {
 	public static void main(String[] args) {
 
 		
-		try (Connection conn = dataSource.getConnection();
-				PreparedStatement pstmt = conn.prepareStatement(SQL_KEY);
+		try (Connection conn = dataSource.getConnection();  //跟 Hikari 連線池借一條 Connection
+				PreparedStatement pstmt = conn.prepareStatement(SQL_KEY);  //把 SQL先準備好
 				){
 			System.out.println("Food Search");
 			System.out.println("-----");
@@ -56,10 +56,10 @@ public class JDBC17 {
 //			
 //			String skey = "%" + key + "%";
 			
-			List<Food> foods = search(page, pstmt);
+			List<Food> foods = search(page, pstmt);  //把第 X 頁和 pstmt 交給 search() 幫我查
 			System.out.printf("共%d筆美食資料\n", foods.size());
 			System.out.println("-----");
-			for (Food food : foods) {
+			for (Food food : foods) {  //foods 裡面的 Food，一個一個拿出來。
 				System.out.printf("%d:%s:%s:%s\n", food.getId(), food.getName(), food.getTel(), food.getAddr());
 				System.out.println("------------------------");
 				
@@ -73,14 +73,14 @@ public class JDBC17 {
 	static List<Food> search(int page, PreparedStatement pstmt)throws Exception{
 		List<Food> foods = new LinkedList<>();
 
-		int start = (page - 1) * rpp;
+		int start = (page - 1) * rpp;  //起始位置 = (頁數 - 1) × 每頁筆數
 		
 		pstmt.setInt(1, start);
 		pstmt.setInt(2, rpp);
 		
-		ResultSet rs = pstmt.executeQuery();
+		ResultSet rs = pstmt.executeQuery();  //SQL 真的送去 MySQL 執行
 		while (rs.next()) {
-			Food food = new Food(rs.getLong("id"), rs.getString("name"), 
+			Food food = new Food(rs.getLong("id"), rs.getString("name"),   //把 ResultSet 轉成 Food 物件
 					rs.getString("city")+rs.getString("town")+rs.getString("addr"),
 					rs.getString("tel"),null);
 			foods.add(food);
